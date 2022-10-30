69cdb19f2746f465323e43df7b4679879c8ea16b
Mert Özler <meozler@gmail.com>
2022-10-30 14:44:18 +0300

    delete unused functions in response

--- src/main/java/com/epara/epara/dto/response/Response.java
+++ src/main/java/com/epara/epara/dto/response/Response.java
@@ -73,23 +73,6 @@ public class Response<T> {
         OK, BAD_REQUEST, VALIDATION_EXCEPTION, EXCEPTION, NOT_FOUND, CREATED
     }
 
-    @Getter
-    @Accessors(chain = true)
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class PageMetadata {
-        private final int size;
-        private final long totalElements;
-        private final int totalPages;
-        private final int number;
-
-        public PageMetadata(int size, long totalElements, int totalPages, int number) {
-            this.size = size;
-            this.totalElements = totalElements;
-            this.totalPages = totalPages;
-            this.number = number;
-        }
-    }
 
 }
 
